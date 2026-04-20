e7bfcccc680f198a13da118e7ab7d9437c5090ee
gioTlahuiz <fcei.tlahuiz.2.e@gmail.com>
2026-04-20 14:28:56 -0600

    método para buscar un usuario por nombre corregido

--- src/com/generation/agenda/services/AgendaServices.java
+++ src/com/generation/agenda/services/AgendaServices.java
@@ -2,12 +2,25 @@ package com.generation.agenda.services;
 
 public class AgendaServices {
     //método para buscar contactos
-    public static Contacto buscarUsuario(List <Contacto> lista, String nombre){
+    public static String buscaContacto(List <Contacto> lista, String nombre){
         for (Contacto c: lista){
-            if(c.nombre == nombre){
-                return c;
+            if(c.getNombre().equalsIgnoreCase(nombre)){
+                return c.getTelefono;
             }
-            return null;
+        }
+        return null;
+    }
+
+    //método para espacios libres
+    public static int espaciosDisponibles(List<Contacto> lista) {
+        int capacidad = 10;
+        int disponibles = capacidad - lista.size();
+        return Math.max(disponibles, 0);
+    }
+
+    public static boolean agendaLlena(List<Contacto> lista){
+        return lista.size() >= 10;
         }
     }
+
 }
