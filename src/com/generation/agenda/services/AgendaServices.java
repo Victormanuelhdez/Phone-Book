ed68d8a039d0a3941a981c931a1449a495ec766b
vianey-lopez <eyvianey@gmail.com>
2026-04-20 16:08:34 -0600

    se agrega método listarContactos

--- src/com/generation/agenda/services/AgendaServices.java
+++ src/com/generation/agenda/services/AgendaServices.java
@@ -1,4 +1,23 @@
 package com.generation.agenda.services;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class AgendaServices {
+    // Listar contactos
+    public void listarContactos() {
+        if (contactos.isEmpty()) {
+            System.out.println("📭 La agenda está vacía.");
+            return;
+        }
+
+        contactos.sort(Comparator.comparing(Contacto::getNombre)
+                .thenComparing(Contacto::getApellido));
+
+        System.out.println("📑 Lista de contactos:");
+        for (Contacto c : contactos) {
+            System.out.println("👤 " + c.getNombre() + " " + c.getApellido() + " - 📞 " + c.getTelefono());
+        }
+    }
+
+
 }
