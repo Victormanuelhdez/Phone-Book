2807c2b674b9ffec6c1335dcb3ea601329dcbcb1
gioTlahuiz <fcei.tlahuiz.2.e@gmail.com>
2026-04-20 14:22:02 -0600

    método para buscar un usuario por nombre

--- src/com/generation/agenda/services/AgendaServices.java
+++ src/com/generation/agenda/services/AgendaServices.java
@@ -1,4 +1,13 @@
 package com.generation.agenda.services;
 
 public class AgendaServices {
+    //método para buscar contactos
+    public static Contacto buscarUsuario(List <Contacto> lista, String nombre){
+        for (Contacto c: lista){
+            if(c.nombre == nombre){
+                return c;
+            }
+            return null;
+        }
+    }
 }
